6f195209e5e1f059f0cef2e1873170cd3a24d3fc
eunbiggabi <eunbiggabi84@gmail.com>
2021-07-27 22:28:54 +1000

    add user css

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
 
+    # validates :name, presence: true, 
+    #                     uniqueness: { case_sensitive: false }, 
+    #                     length: { minimum: 6, maximum: 200 }
+
+    # validates :description, presence: true,  
+    #                     length: { minimum: 6, maximum: 5000 }
+
     has_one_attached :picture
 end
